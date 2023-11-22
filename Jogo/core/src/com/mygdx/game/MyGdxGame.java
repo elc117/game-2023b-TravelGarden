668e90139cd1d2d69e2f30c87b29e6ec538ddfa2
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-11-21 22:39:22 -0300

    Update MyGdxGame.java

--- Jogo/core/src/com/mygdx/game/MyGdxGame.java
+++ Jogo/core/src/com/mygdx/game/MyGdxGame.java
@@ -4,15 +4,29 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.GL20;
+
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font; 
+	String[] options; 
+	String question; 
+	int correctOption;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("menina.jpg");
+		font = new BitmapFont();
+
+		question = "Qual é o bioma na cidade de Santa Maria?";
+		options = new String[]{"Floresta Atlantica","Pampas", "Cerrado","Mediterraneo"};
+		correctOption = 1; 
 	}
 
 	@Override
@@ -20,7 +34,33 @@ public class MyGdxGame extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
+
+		/////////////////////////
+
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+   	      ScreenUtils.clear(1, 0, 0, 1);
+
+
+		
 		batch.end();
+		
+		 // Lógica para verificar a resposta ao tocar na tela
+		 if (Gdx.input.isTouched()) {
+        	float touchX = Gdx.input.getX();
+        	float touchY = Gdx.input.getY();
+
+        	// Verifique se o toque está dentro da área da opção correta
+        	if (touchX > 100 && touchX < 500 && touchY > 200 && touchY < 250) {
+            	// Implemente o que acontece quando a resposta está correta
+            	Gdx.app.log("QuizGame", "Resposta correta!");
+        	} else {
+            	// Implemente o que acontece quando a resposta está incorreta
+            	Gdx.app.log("QuizGame", "Resposta incorreta!");
+        	}
+
+		 }
 	}
 	
 	@Override
