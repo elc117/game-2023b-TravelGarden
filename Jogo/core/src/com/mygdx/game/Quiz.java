daab3b8a13055c20444c3e408478420ba8bcd9db
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-11-29 19:36:14 -0300

    Update Quiz.java

--- Jogo/core/src/com/mygdx/game/Quiz.java
+++ Jogo/core/src/com/mygdx/game/Quiz.java
@@ -37,7 +37,7 @@ public class Quiz extends ApplicationAdapter implements Screen{
 
         question = "Qual é a capital do Brasil?";
         options = new String[]{"Brasília", "Rio de Janeiro", "São Paulo", "Belo Horizonte"};
-        correctOption = 0;  // A opção correta, neste caso, é "Brasília"
+        correctOption = 0;  
 
         stage = new Stage();
 
@@ -46,20 +46,20 @@ public class Quiz extends ApplicationAdapter implements Screen{
         nextButtonStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture("botao.png")));
         nextButton = new ImageButton(nextButtonStyle);
         nextButton.setPosition(Gdx.graphics.getWidth() / 2 - nextButton.getWidth() / 2, 50);
-        nextButton.setVisible(false);  // Inicialmente, mantenha o botão invisível
+        nextButton.setVisible(false);  
         nextButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Avance para a próxima tela (substitua ProximaTela pela classe da próxima tela)
+              
                 Gdx.app.log("QuizGame", "Avançando para a próxima tela...");
                 game.setScreen(new MyGdxGame(game));
-                // Libere os recursos da tela atual
+               
                 dispose();
             }
         });
         stage.addActor(nextButton);
 
-        // Configuração do palco como processador de entrada
+     
         Gdx.input.setInputProcessor(stage);
     }
 
@@ -80,30 +80,30 @@ public class Quiz extends ApplicationAdapter implements Screen{
 
         batch.end();
 
-        // Lógica para verificar a resposta ao tocar na tela
+        
         if (Gdx.input.isTouched()) {
             float touchX = Gdx.input.getX();
             float touchY = Gdx.input.getY();
             Gdx.app.log("QuizGame", "Touch X: " + touchX + ", Touch Y: " + touchY);
 
 
-            // Verifique se o toque está dentro da área da opção correta
+            
             if (touchX > 88 && touchX < 170 && touchY > 500 && touchY < 650) {
-                // Implemente o que acontece quando a resposta está correta
+                
                 Gdx.app.log("QuizGame", "Resposta correta!");
-                // Marque o quiz como concluído
+           
                 quizConcluido = true;
-                // Torna o botão de avanço visível
+              
                 nextButton.setVisible(true);
             } else {
-                // Implemente o que acontece quando a resposta está incorreta
+              
                 Gdx.app.log("QuizGame", "Resposta incorreta!");
             }
         }
 
-        // Atualize o palco
+      
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        // Desenhe o palco
+        
         stage.draw();
     }
 
@@ -111,7 +111,7 @@ public class Quiz extends ApplicationAdapter implements Screen{
     public void dispose() {
         batch.dispose();
         img.dispose();
-        // Libere os recursos da textura do botão
+ 
         
     }
 
